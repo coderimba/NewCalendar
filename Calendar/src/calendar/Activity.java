a6002da85c3371971f305c7c048c1a975575ac30
Mitchell Tanbun <mitchellt@berkeley.edu>
2020-01-02 19:19:08 -0800

    Additions in Activity.java and Day.java:
    Activity.java:
    + Added skeleton for constructors and toString with descriptions
    + Added member field for description of the activity

--- Calendar/src/calendar/Activity.java
+++ Calendar/src/calendar/Activity.java
@@ -1,10 +1,11 @@
 package calendar;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * An activity object containing a reminder, start and end time, and priority
- * level.
+ * level. Each reminder is associated with a <code>Day</code>.
  *
  * @author Mitchell Tanbun
  */
@@ -13,16 +14,68 @@ public class Activity {
     private enum PRIORITY_LEVELS {NONE, LOW, MED, HIGH}
     
     /** Priority level of the task. */
-    private PRIORITY_LEVELS _priority;
+    private PRIORITY_LEVELS _priority = PRIORITY_LEVELS.NONE;
+    
+    /** Description of the activity. */
+    private String _description;
     
     /**
      * Start and end time of this activity. If any of the fields are
      * unspecified, then take the start and end to be 0000 hrs and 2359 hrs
      * of the Pacific Standard Time (PST) respectively.
+     *
+     * @see java.util.Calendar
      */
     private Calendar _start, _end;
     
     /**
+     * Creates a new reminder with the current time as the start and 23 59
+     * on today's date as the end.
      *
+     * @param description The activity to be reminded of
      */
+    Activity(String description) {
+        ;
+    }
+    
+    /**
+     * Creates a new reminder with the specified ending time
+     *
+     * @param description The activity to be reminded of
+     * @param endTime     The ending time of the activity
+     */
+    Activity(String description, Calendar endTime) {
+    
+    }
+    
+    /**
+     * Creates a new reminder with the corresponding starting and ending time
+     *
+     * @param description The activity to be reminded of
+     * @param start       The starting time
+     * @param end         The ending time
+     */
+    Activity(String description, Calendar start, Calendar end) {
+    
+    }
+    
+    /**
+     * Creates a new reminder with all member fields filled in.
+     *
+     * @param description Description of the activity
+     * @param start       Starting time
+     * @param end         Ending time
+     * @param pr          The level of importance of the reminder
+     */
+    Activity(String description, Calendar start, Calendar end,
+             PRIORITY_LEVELS pr) {
+        
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder data = new StringBuilder();
+        // TODO
+        return data.toString();
+    }
 }
--- Calendar/src/calendar/Day.java
+++ Calendar/src/calendar/Day.java
@@ -1,18 +1,22 @@
 package calendar;
 
+import java.io.Serializable;
+import java.io.File;
+
 import java.lang.String;
+
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.regex.Pattern;
 
+// TODO: Maybe custom labels could be added to this diary entry.
 /**
  * A day entry in a diary which contains today's date and a reminder, along
- * with the level of importance of the task. // TODO: Maybe custom labels could
- * be added to this diary entry.
+ * with the level of importance of the task.
  *
  * @author Mitchell Tanbun
  */
-public class Day {
+public class Day implements Serializable {
     /** Day of the month. */
     private byte _day;
     
@@ -30,7 +34,7 @@ public class Day {
      */
     public static final Pattern DATE_REGEX = Pattern.compile(
         "(" + "([12][0-9]|[0][1-9])|[3][01]" + ")/"
-        + "(" + "([0][1-9]|[1][012])" + ")/"
+        + "(" + "[0][1-9]|[1][012]" + ")/"
         + "(" + "\\d{4}" + ")");
     
     /** Constructor of a day object. */
@@ -52,5 +56,10 @@ public class Day {
         return (1 <= day && day <= MAX_DAY);
     }
     
-    /** */
+    /**
+     * Adds a REMINDER to today's date.
+     */
+    private void addActivity(Activity reminder) {
+    
+    }
 }
