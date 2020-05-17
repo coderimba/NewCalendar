b7c3ea9b2664ee87cb4f225440955a332839fe3f
Mitchell Tanbun <48144326+mitan1997@users.noreply.github.com>
2020-05-17 11:26:20 -0700

    Added static variables for storing number of days in each month.
    Month.java:
    + Added instance attribute _currentMonth to store the current month.
    + Added number of days per month in a standard non-leap year.
    * Corrected variable naming convention to use camelCase.
    * Corrected logic error for determining number of days in the month.
    * Used 1-indexed convention for month as an argument for constructor.

--- Calendar/src/calendar/Month.java
+++ Calendar/src/calendar/Month.java
@@ -1,37 +1,42 @@
 package calendar;
 
-import java.util.*;
-import java.lang.*;
+import calendar.Day;
+
+import java.util.Arraylist;
 
 /** A collection of dates in the month. */
 public class Month {
     /** Possible months. */
-    private enum PossibleMonths {JAN, FEB, MAR, APR, MAY, JUN,
-        JUL, AUG, SEP, OCT, NOV, DEC};
-
+    private enum PossibleMonths {
+        JAN, FEB, MAR, APR, MAY, JUN,
+        JUL, AUG, SEP, OCT, NOV, DEC
+    };
+    
+    /** Number of dates in each month. */
+    private static int NumDays[] = {31, 28, 31, 30, 31, 30,
+                                    31, 31, 30, 31, 30, 31};
+    
     /** Current month. */
     private PossibleMonths _currentMonth;
-
+    
     /** ArrayList of Days */
-    private ArrayList<Day> Day_ls;
-
-    /** Takes in current month's NUMBER. */
+    protected ArrayList<Day> dayLst;
+    
+    /**
+     * Takes in current month's number.
+     *
+     * @param month The one-indexed month in which the constructor is called
+     */
     public Month(int month) {
         // TODO: Constructs an object with all days in the month initialized.
-     if (!validMonth(month)) {
-         throw new IllegalArgumentException("Month is out of range.");
-     } else if (month % 2 == 0) {
-         for (int i = 1; i < 32; i++) {
-            Day_ls.add(new Day(i));
-         }
-     } else {
-         for (int i = 1; i< 31; i++) {
-            Day_ls.add(new Day(i));
-         }
-     } 
+        if (!validMonth(month)) {
+            throw new IllegalArgumentException("Month is out of range.");
+        }
+        dayLst = new ArrayList<Day>(NumDays[month - 1]);
+        _currentMonth = PossibleMonths.values()[month - 1];
     }
-
-    /** Returns whether*/
+    
+    /** Returns whether the month is valid */
     private boolean validMonth(int month) {
         // TODO: Current month checker
         return (month >= 1 && month <= 12);
