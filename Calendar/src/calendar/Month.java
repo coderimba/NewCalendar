a2cbbaef239c2c2572d5872aadb9c5af44bc6bd2
tartwig <56523608+tartwig@users.noreply.github.com>
2020-05-18 15:23:20 +0800

    Update Month.java

--- Calendar/src/calendar/Month.java
+++ Calendar/src/calendar/Month.java
@@ -18,22 +18,48 @@ public class Month {
     /** Takes in current month's NUMBER. */
     public Month(int month) {
         // TODO: Constructs an object with all days in the month initialized.
-     if (!validMonth(month)) {
-         throw new IllegalArgumentException("Month is out of range.");
-     } else if (month % 2 == 0) {
-         for (int i = 1; i < 32; i++) {
-            Day_ls.add(new Day(i));
-         }
-     } else {
-         for (int i = 1; i< 31; i++) {
-            Day_ls.add(new Day(i));
-         }
-     } 
+    	switch(month) {
+     		case 1: days_31(); break;
+     		case 2: feb(); break;
+     		case 3: days_31(); break;
+     		case 4: days_30(); break;
+     		case 5: days_31(); break;
+     		case 6: days_30(); break;
+     		case 7: days_31(); break;
+     		case 8: days_31(); break;
+     		case 9: days_30(); break;
+     		case 10: days_31(); break;
+     		case 11: days_30(); break;
+     		case 12: days_31(); break;
+     		default: throw new IllegalArgumentException("Month is out of range.");
+    	}
     }
 
     /** Returns whether*/
-    private boolean validMonth(int month) {
+    public boolean validMonth(int month) {
         // TODO: Current month checker
         return (month >= 1 && month <= 12);
+    }
+    /* Helper methods */
+    /* 31-day helper method */
+    public void days_31() {
+    	for (int i = 1; i < 32; i++) 
+            Day_ls.add(new Day(i));
+    }
+    /* 30-day helper method */
+    public void days_30() {
+    	for (int i = 1; i < 31; i++)
+            Day_ls.add(new Day(i));
+    }
+    /* February helper method */
+    public void feb() {
+    	int year = Calendar.getInstance().get(Calendar.YEAR); //get current year as an int
+    	if (year % 4 == 0) {
+    		for (int i = 1; i < 30; i++)	//leap years (29 days in Feb)
+                Day_ls.add(new Day(i));
+    	} else {
+    		for (int i = 1; i < 29; i++)	//other years (28 days in Feb)
+                Day_ls.add(new Day(i));
+    	}
     }
 }
