d38bc5b773f3712aedec86f98fb1400a54f6cc13
Mitchell Tanbun <48144326+mitan1997@users.noreply.github.com>
2020-05-23 20:08:02 -0700

    Slight modifications to structure.
    Week.java:
    + Added structure to get number of days elapsed since app was last opened, but assumes that the year number does not change.
    + Added instance attribute firstDate to keep track of the exact date of the first object since the `Day` object in the LinkedList only tracks the day of month aspect of a date.

--- Calendar/src/calendar/Week.java
+++ Calendar/src/calendar/Week.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.util.Calendar;
 import java.util.LinkedList;
 
+import java.time.temporal.ChronoUnit;
+
 /**
  * Week object to hold dates and reminders within those dates in an
  * easily-accessible fashion. This object uses the {@link Calendar} object to
@@ -18,6 +20,9 @@ import java.util.LinkedList;
 public class Week implements Serializable {
     /** Number of days in a week */
     private static final int NUM_DAYS_IN_WEEK = 7;
+    
+    /** The day where the app was last opened. */
+    private Calendar firstDay;
     
     /**
      * Creates a `LinkedList` of days, modelled as a queue for easily
@@ -55,7 +60,8 @@ public class Week implements Serializable {
      */
     public void setCalendar() {
         //getInstance() returns instance of calendar
-        Calendar currTime = Calendar.getInstance();
+        firstDay = Calendar.getInstance();
+        Calendar currTime = firstDay;
         // using current time zone
     }
     
@@ -64,9 +70,20 @@ public class Week implements Serializable {
      * the next few numbers of {@link Day} objects could be enqueued in the
      * {@link LinkedList}. Returns false if no updates to the queue of days
      * were needed.
+     *
+     * @see LinkedList#addFirst(Object)
+     * @see LinkedList#removeFirst()
+     *
+     * @see Calendar#get(int)
+     * @see Calendar#DAY_OF_YEAR
+     *
      */
     protected boolean requeue() {
-        // TODO
+        // TODO: Add handling of corner cases where the week goes over the
+        //  year.
+        Calendar currDate = Calendar.getInstance();
+        int numDays = currDate.get(Calendar.DAY_OF_YEAR) -
+            firstDay.get(Calendar.DAY_OF_YEAR);
         return false;
     }
     
